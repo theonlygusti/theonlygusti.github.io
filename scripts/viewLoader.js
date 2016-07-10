08928f70337895debb7b76bbcef750b8de75e513
BuildTools <theonlygusti@gmail.com>
2016-07-10 13:24:40 +0100

    Adding more view stuff.

--- scripts/viewLoader.js
+++ scripts/viewLoader.js
@@ -8,5 +8,10 @@ function loadView(view) {
   var icon = document.createElement("img");
   icon.src = "http://i.cubeupload.com/EKlJJO.png";
   document.body.appendChild(icon);
+
+  var title = document.createelement("img");
+  title.src = "http://i.cubeupload.com/ZEbcCS.png";
+  document.body.appendChild(title);
+  
   document.body.setAttribute("style", "background-image:url(http://photos1.meetupstatic.com/photos/theme_body/a/1/3/d/full_6881277.jpeg)");
 }
